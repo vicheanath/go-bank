dce2347f2048c466b239c43dc23a75b8ecea46b6
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-11-07 19:13:18 -0600

    Added CRUD migration to generate sqlc

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -10,6 +10,10 @@ import (
 
 type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	DeleteAccount(ctx context.Context, id int64) error
+	GetAccount(ctx context.Context, id int64) (Account, error)
+	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
+	UpdateAccount(ctx context.Context, arg UpdateAccountParams) error
 }
 
 var _ Querier = (*Queries)(nil)
